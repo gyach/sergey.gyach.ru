05fa9e1ad44467db821116e367d22e644af36bb3
Sergey Gyach <sergey@gyach.ru>
2026-06-08 14:56:19 +0300

    Add Yandex Metrika tracking to root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,6 +38,15 @@ const themeInitScript = `(() => {
   }
 })();`;
 
+const yandexMetrikaScript = `(function(m,e,t,r,i,k,a){
+  m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+  m[i].l=1*new Date();
+  for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+  k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+})(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=109724855', 'ym');
+
+ym(109724855, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});`;
+
 export const metadata: Metadata = {
   metadataBase: new URL(site.url),
   title: {
@@ -102,6 +111,21 @@ export default function RootLayout({
           strategy="beforeInteractive"
           dangerouslySetInnerHTML={{ __html: themeInitScript }}
         />
+        <Script
+          id="yandex-metrika"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{ __html: yandexMetrikaScript }}
+        />
+        <noscript>
+          <div>
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img
+              src="https://mc.yandex.ru/watch/109724855"
+              style={{ position: "absolute", left: "-9999px" }}
+              alt=""
+            />
+          </div>
+        </noscript>
         {children}
       </body>
     </html>
